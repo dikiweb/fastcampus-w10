99f4ec1ddd64ac0d93cceb8daea9a7541ab2b981
Park Jongchul (DIKI) <maxjc@naver.com>
2024-06-01 18:55:08 +0900

    #6 - SpringBoot 프로젝트 시작
    SpringBoot 프로젝트 초기화
    
    [포함한 의존성 패키지]
    * Spring Web Web [Web]
    * Lombok [Developer Tools]
    * Spring Boot DevTools [Developer Tools]
    * Spring Boot Actuator [Ops]

---
+++ w10-project-board/src/test/java/com/festcampus/w10_project_board/W10ProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festcampus.w10_project_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W10ProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
