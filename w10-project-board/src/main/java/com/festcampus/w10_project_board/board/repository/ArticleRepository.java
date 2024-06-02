5d3d3c299f0d95901d6d85ab1f5c3727ccb8faf2
Park Jongchul (DIKI) <maxjc@naver.com>
2024-06-02 22:24:07 +0900

    #1 - Querydsl 지원을 이용해서 api 검색 옵션 추가
    * QuerydslPredicateExecutor
    * QuerydslBinderCustomizer
       QuerydslBinderCustomizer는 @Override ... 해야 함

--- w10-project-board/src/main/java/com/festcampus/w10_project_board/board/repository/ArticleRepository.java
+++ w10-project-board/src/main/java/com/festcampus/w10_project_board/board/repository/ArticleRepository.java
@@ -1,8 +1,16 @@
 package com.festcampus.w10_project_board.board.repository;
 
 import com.festcampus.w10_project_board.common.entity.Article;
+import com.festcampus.w10_project_board.common.entity.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.time.DateTimeException;
 
 /**
  * packageName   : com.festcampus.w10_project_board.board.repository
@@ -16,6 +24,18 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * 2024-06-01          danny         최초 생성
  */
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
