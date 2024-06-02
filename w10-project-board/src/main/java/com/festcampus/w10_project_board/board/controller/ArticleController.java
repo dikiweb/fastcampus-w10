4de8c5836f4fbef7d0f2c21e71415dd0a56df52f
Park Jongchul (DIKI) <maxjc@naver.com>
2024-06-02 23:22:20 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열지 테스트로 명세를 뽑는다

---
+++ w10-project-board/src/main/java/com/festcampus/w10_project_board/board/controller/ArticleController.java
@@ -0,0 +1,20 @@
+package com.festcampus.w10_project_board.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * packageName   : com.festcampus.w10_project_board.board.controller
+ * fileName     : ArticleController
+ * author       : danny
+ * date         : 2024-06-02
+ * description  :
+ * ===========================================================
+ * DATE             AUTHOR          NOTE
+ * -----------------------------------------------------------
+ * 2024-06-02          danny         최초 생성
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
