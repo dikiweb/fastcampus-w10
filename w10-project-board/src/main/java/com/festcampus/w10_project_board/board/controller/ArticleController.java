45227b41c72a94f216d3c1904928e711a6c9a4c1
Park Jongchul (DIKI) <maxjc@naver.com>
2024-06-03 10:41:18 +0900

    #16 - 게시판 뷰 기본골격 작성
    * Controller 가 테스트를 통과하도록 열어줌
       : .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
    * ArticleController에 list 반영
    * Html 페이지 의미 코딩

--- w10-project-board/src/main/java/com/festcampus/w10_project_board/board/controller/ArticleController.java
+++ w10-project-board/src/main/java/com/festcampus/w10_project_board/board/controller/ArticleController.java
@@ -1,7 +1,11 @@
 package com.festcampus.w10_project_board.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * packageName   : com.festcampus.w10_project_board.board.controller
@@ -17,4 +21,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/list";
+    }
 }
--- w10-project-board/src/test/java/com/festcampus/w10_project_board/board/controller/ArticleControllerTest.java
+++ w10-project-board/src/test/java/com/festcampus/w10_project_board/board/controller/ArticleControllerTest.java
@@ -32,7 +32,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -41,7 +40,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/list"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -55,7 +54,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("articles"))
                 .andExpect(model().attributeExists("articlesComments"));
@@ -70,7 +69,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
 
     }
@@ -84,7 +83,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
 
     }
